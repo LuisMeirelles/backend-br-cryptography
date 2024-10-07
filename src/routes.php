d0ea1761a390bf9d88dd4a6b442b611ddadbba6c
Luis Meirelles <luis.gustavo@essentia.com.br>
2024-10-06 22:29:40 -0300

    feat: implement a `get user by id` endpoint

--- src/routes.php
+++ src/routes.php
@@ -3,5 +3,7 @@
 global $router;
 
 use Meirelles\BackendBrCryptography\User\Controllers\CreateUser;
+use Meirelles\BackendBrCryptography\User\Controllers\GetUser;
 
 $router->post('/users', new CreateUser());
+$router->get('/users/:id', new GetUser());
